50e8bb0f081332004fac49bb6446050968266a64
roshanspanilk <roshang@spanilk.com>
2021-12-08 20:57:30 +0545

    Creation of Body

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+// // import { GlobeIcon } from "@heroicons/react";
+// function Footer() {
+//   return (
+//     <footer>
+//       <div>
+//         <p>Nepal</p>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
